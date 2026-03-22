0432573960e15d1a46718620c8516fdcb95c2d41
wydnickk <nnamazova374@gmail.com>
2026-03-22 13:59:42 +0000

    hi

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,5 +1,7 @@
+#!/usr/bin/python3
 def islower(c):
+    if type(c) is not str or len(c) != 1:
+        return False
     if ord(c) >= ord('a') and ord(c) <= ord('z'):
         return True
-    else:
-        return False
+    return False
