4ba3f86241e211c918ef6c3e3a38d031dadb98b8
wydnickk <nnamazova374@gmail.com>
2026-04-12 20:26:06 +0400

    final fix base geometry doctest

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-"""Module containing empty class"""
+"""Module containing BaseGeometry class"""
 
 
 class BaseGeometry:
     """Class for BaseGeometry"""
+
     def area(self):
         """Method to raise exception"""
         raise Exception("area() is not implemented")
@@ -12,7 +13,7 @@ class BaseGeometry:
         """Method to validate integer"""
 
         if type(value) is not int:
-            raise TypeError(name + " must be an integer")
+            raise TypeError(f"{name} must be an integer")
 
         if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+            raise ValueError(f"{name} must be greater than 0")
